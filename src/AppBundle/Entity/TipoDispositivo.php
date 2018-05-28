e7ec05fd572724b05b989d97f889d9dea60c2848
agamaru <agamaru@gmail.com>
2018-05-28 18:50:37 +0200

    Añadida entidad TipoDispositivo

---
+++ src/AppBundle/Entity/TipoDispositivo.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elsiore
+ * Date: 20/05/18
+ * Time: 7:44
+ */
+
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="tipo_dispositivo")
+ */
+class TipoDispositivo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    private $nombre;
+
+    public function __toString()
+    {
+        return $this->getNombre();
+    }
+
+    /// Getters y setters
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param string $nombre
+     * @return TipoDispositivo
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+}
