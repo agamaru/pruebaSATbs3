3a5dbe19ef2abc6f86ec1c5cced84f8d971852b6
agamaru <agamaru@gmail.com>
2018-05-29 17:16:29 +0200

    Servidor: Implementado formulario altas, modificaciones y bajas (SIN restricciones)

--- src/AppBundle/Controller/ServidorController.php
+++ src/AppBundle/Controller/ServidorController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Servidor;
+use AppBundle\Form\Type\ServidorType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ServidorController extends Controller
 {
@@ -16,6 +19,66 @@ class ServidorController extends Controller
 
         return $this->render('servidor/listar.html.twig', [
             'servidores' => $servidores
+        ]);
+    }
+
+    /**
+     * @Route("/servidor/editar/nuevo", name="servidor_nuevo")
+     * @Route("/servidor/editar/{id}", name="servidor_editar")
+     */
+    public function crearAction(Request $request, Servidor $servidor = null)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $nuevo = false;
+
+        if (null === $servidor) {
+            $nuevo = true;
+            $servidor = new Servidor();
+            $em->persist($servidor);
+        }
+
+        $form = $this->createForm(ServidorType::class, $servidor, [
+            'nuevo' => $nuevo,
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $em->flush();
+                $this->addFlash('info', 'Cambios guardados');
+            } catch (\Exception $e) {
+                $this->addFlash('error', 'No se han podido guardar los cambios');
+            }
+            return $this->redirectToRoute('servidor_listar');
+        }
+
+        return $this->render('servidor/form.html.twig', [
+            'servidor' => $servidor,
+            'formulario' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/servidor/eliminar/{id}", name="servidor_eliminar")
+     */
+    public function eliminarAction(Request $request, Servidor $servidor)
+    {
+        if ($request->isMethod('POST')) {
+            $em = $this->getDoctrine()->getManager();
+            try {
+                $em->remove($servidor);
+                $em->flush();
+                $this->addFlash('info', 'El servidor ha sido eliminado');
+            } catch (\Exception $e) {
+                $this->addFlash('error', 'No se ha podido eliminar el servidor');
+            }
+            return $this->redirectToRoute('servidor_listar');
+        }
+
+        return $this->render('servidor/eliminar.html.twig', [
+            'servidor' => $servidor
         ]);
     }
 }
---
+++ src/AppBundle/Form/Type/ServidorType.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elsiore
+ * Date: 22/05/18
+ * Time: 19:38
+ */
+
+namespace AppBundle\Form\Type;
+
+
+use AppBundle\Entity\Empresa;
+use AppBundle\Entity\Servidor;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ServidorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if (false === $options['nuevo']) {
+            $builder
+                ->add('empresa', null, [
+                    'label' => 'Empresa',
+                    'disabled' => true
+                ]);
+        } else {
+            $builder
+                ->add('empresa', EntityType::class, [
+                    'class' => Empresa::class,
+                    'query_builder' => function (EntityRepository $er) {
+                        return $er->createQueryBuilder('e')
+                            ->orderBy('e.nombre');
+                    },
+                    'label' => 'Empresa *'
+                ]);
+        }
+
+        $builder
+            ->add('nombre', null, [
+                'label' => 'Nombre *'
+            ])
+            ->add('ip', null, [
+                'label' => 'IP *'
+            ])
+
+            ->add('usuario', null, [
+                'label' => 'Usuario *'
+            ])
+            ->add('pass', null, [
+                'label' => 'Contraseña *'
+            ])
+            ->add('detalles', null, [
+                'label' => 'Recursos de red y directivas *'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Servidor::class,
+            'nuevo' => false
+        ]);
+    }
+}
